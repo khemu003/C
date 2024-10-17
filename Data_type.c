61f8e1ca35bea915c97f3cecfb125be862caf339
khemraj mangal <135795862+khemu003@users.noreply.github.com>
2024-10-17 22:59:44 +0530

    Create Data_type.c

---
+++ Data_type.c
@@ -0,0 +1,38 @@
+
+-> print 67
+#include<stdio.h>
+int main(){
+    int a = 67; //int
+    printf("%d", a);
+    return 0;
+}
+
+-> print 9.86
+#include<stdio.h>
+int main(){
+    float a = 9.86; //float
+    printf("%f", a);
+    return 0;
+}
+
+-> print 'n'
+#include<stdio.h>
+int main(){
+    char a = 'n'; //character
+    printf("%c", a);
+    return 0;
+}
+
+-> what is data type ?
+	->data type use to store the variable which type of value stored in variable.
+
+-> what is variable ?
+	->variable is like container that is used to stored and hold the value(data)
+	->it provide memory.
+
+-> types of data type ? 
+	->there are two type
+		->A) primitive data type
+			->like int, float, char etc.
+		->B) non-primitive data type
+			->like string, array, pointer, etc.
