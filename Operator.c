c4f323b7f5bbda73734b42a326328f00bd08f437
khemraj mangal <135795862+khemu003@users.noreply.github.com>
2024-11-08 23:09:57 +0530

    Update Operator.c

--- Operator.c
+++ Operator.c
@@ -48,7 +48,18 @@ symbol :
 == :equal equal to 
 != :not equal to 
 */
-
+#include <stdio.h>
+int main() {
+  int x = 5;
+  int y = 3;
+  printf("%d\n", x > y); // Returns 1 (true) because 5 is greater than 3
+  printf("%d\n", x < y); // Returns 0 (false) because 5 is not less than 3
+  printf("%d\n", x >= y); // Returns 1 (true) because five is greater than, or equal, to 3
+  printf("%d\n", x <= y); // Returns 0 (false) because 5 is neither less than or equal to 3
+  printf("%d\n", x != y); // returns 1 (true) because 5 is not equal to 3
+  printf("%d\n", x == y); // returns 1 (true) because 5 is equal to 5
+  return 0;
+}
 /*
 ---------------------------------------------------------
 3)assignment operator :
